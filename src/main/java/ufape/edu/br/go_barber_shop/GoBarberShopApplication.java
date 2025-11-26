9187bbc488939eaa4aaf4b6efc13e90788288592
Mágno Sillas <magno.sillas0@gmail.com>
2025-11-25 21:42:29 -0300

    first commit

---
+++ src/main/java/ufape/edu/br/go_barber_shop/GoBarberShopApplication.java
@@ -0,0 +1,13 @@
+package ufape.edu.br.go_barber_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoBarberShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoBarberShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ufape/edu/br/go_barber_shop/GoBarberShopApplicationTests.java
@@ -0,0 +1,13 @@
+package ufape.edu.br.go_barber_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoBarberShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
